d9bfafbf8f77c5666199e114e2ef2788951534e8
zaiats85 <oleg.zaiats@gmail.com>
2019-03-16 01:08:35 +0200

    parse data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 /*INPUT & COFIG*/
 const method = "GET";
 const url = "data.json";
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+const WIDHT = 1200;
+const HEIGHT = 800;
 
 /*TRANSPORT*/
-const getJson = (method, url) => {
+function getJson(method, url) {
     return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url);
-        xhr.onload =  () => {
-            if (this.status >= 200 && this.status < 300) {
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
                 resolve(xhr.response);
             } else {
                 reject({
@@ -25,32 +28,81 @@ const getJson = (method, url) => {
         };
         xhr.send();
     })
-};
+}
 
 /* UTILS */
+const createCanvas = (width, height, id) => {
+    let canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    canvas.id = id;
+    return canvas
+};
 
-function init() {
+const getDate = timestamp => {
+    let date = new Date(timestamp);
+    return months[date.getMonth()];
+};
 
-    debugger
-
-    getJson(method, url)
-        .then(function (result) {
-            console.log(JSON.parse(result));
-        })
-        .catch(function (err) {
-            console.error('An error!', err.statusText);
-        })
+/*RUNTIME*/
+async function init() {
+    let data = await getJson(method, url);
+    return JSON.parse(data);
 }
 
-init();
+init()
+    .then(result => {
+        parseFeed(result[0])
+    });
 
-function component() {
-    let element = document.createElement('div');
+const parseFeed = (feed) => {
+    const graphs = [];
+    const {colors, names, types, columns} = feed;
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerText = "Hello world";
+    /*CANVAS*/
+    const mainImg = createCanvas(WIDHT, HEIGHT, 'mainImg');
+    let ctx = mainImg.getContext("2d");
+    ctx.scale(1, 1);
 
-    return element;
-}
+    const drawGraph = (input) => {
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.strokeStyle = input.color;
+        ctx.lineJoin = 'round';
 
-document.body.appendChild(component());
+        const {x, y} = input.data;
+
+        for (let i = 0; i < x.length; i++) {
+            ctx.lineTo(i*10, y[i]);
+        }
+
+        ctx.stroke();
+
+    };
+
+    Object.entries(names).forEach(([key, value]) => {
+        let graph = {
+            color: colors[key],
+            name: names[key],
+            type: types[key],
+            data: {
+                x: columns.find(col => col.includes("x")),
+                y: columns.find(col => col.includes(key))
+            }
+        };
+
+        graphs.push(graph);
+        drawGraph(graph);
+    });
+
+    end(mainImg);
+
+};
+
+const deleteGraph = (data) => {
+
+};
+
+const end = (mainImg) => {
+    document.body.appendChild(mainImg);
+};
