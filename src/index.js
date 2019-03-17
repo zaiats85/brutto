7be0274acb71c09f11b1912f9e3647ca1d4569d3
zaiats85 <oleg.zaiats@gmail.com>
2019-03-17 13:02:15 +0200

    add ability to toggle graphs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import './style.scss';
+
 /*INPUT & COFIG*/
 const method = "GET";
 const url = "data.json";
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-const WIDHT = 1200;
-const HEIGHT = 800;
-import './style.scss';
+const canavsSize = { width: 1200, height: 800 };
+const buttonSize = { width: "140px", height: "50px" };
+
+let controls = document.getElementById("controls");
+let main = document.getElementById("main");
 
 /*TRANSPORT*/
 function getJson(method, url) {
@@ -32,13 +36,21 @@ function getJson(method, url) {
 }
 
 /* UTILS */
-const createCanvas = (width, height, id) => {
+const createCanvas = ({ width, height }, id) => {
     let canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
     canvas.id = id;
     return canvas
 };
+
+const objectWithoutKey = (object, key) => {
+    const {[key]: deletedKey, ...otherKeys} = object;
+    return otherKeys;
+};
+
+// deep clone
+const deepClone = (input) =>  JSON.parse(JSON.stringify(input));
 
 const getDate = timestamp => {
     let date = new Date(timestamp);
@@ -57,21 +69,78 @@ init()
     });
 
 const parseFeed = (feed) => {
-    let graphs = [];
+    let graphs = {};
+    let cachedGraph = {};
+    let buttons = {};
     const {colors, names, types, columns} = feed;
 
     /*CANVAS*/
-    const mainImg = createCanvas(WIDHT, HEIGHT, 'mainImg');
+    const mainImg = createCanvas(canavsSize, 'mainImg');
     let ctx = mainImg.getContext("2d");
 
+    const init = () => {
+        // Form Graphs n Buttons
+        Object.entries(names).forEach(([key, value]) => {
+            /*@TODO add new Graph() es6 class*/
+            graphs[key] = {
+                color: colors[key],
+                name: names[key],
+                type: types[key],
+                data: {
+                    x: columns.find(col => col.includes("x")),
+                    y: columns.find(col => col.includes(key))
+                }
+            };
+            /*@TODO add new Button() es6 class*/
+            buttons[key] = {
+                color: colors[key],
+                id: key,
+                label: value,
+                size: buttonSize
+            };
+        });
+
+        //interesting approach to manipulate scene redraw, at least for me
+        cachedGraph = deepClone(graphs);
+
+        draw();
+        end(mainImg);
+    };
+
+    //clear feed canvas
+    const clearCanvas = () => {
+        const { width, height } = canavsSize;
+        ctx.clearRect(0, 0, width, height);
+    };
+
+    //delete graph from feed canvas
+    const toggleGraph = (evt) => {
+        //clear the feed canvas
+        clearCanvas();
+        const key = evt.target.id;
+        const tmp = cachedGraph[key];
+
+        if(graphs[key]){
+            //delete the graph
+            graphs = objectWithoutKey(graphs, key);
+        } else {
+            //add the graph
+            graphs[key] = tmp;
+        }
+
+        //redraw the scene
+        draw()
+    };
+
+    //create single graph
     const drawGraph = (input) => {
+        const { color,  data:{ x, y } }  = input;
         ctx.lineWidth = 3;
         ctx.beginPath();
-        ctx.strokeStyle = input.color;
+        ctx.strokeStyle = color;
         ctx.lineJoin = 'round';
 
-        const { x, y } = input.data;
-
+        // LINES
         for (let i = 0, k = x.length; i < k; i++) {
             ctx.lineTo(i*10, y[i]);
         }
@@ -80,53 +149,30 @@ const parseFeed = (feed) => {
 
     };
 
-    const drawControls = (key, value, color) => {
+    const drawButton = ({id, color, label, size: { width, height }}) => {
         let button = document.createElement('button');
-        button.id = key;
-        button.innerText = value;
+        button.id = id;
+        button.innerText = label;
         button.style.background = color;
-        button.style.width = "140px";
-        button.style.height = "50px";
+        button.style.width = width;
+        button.style.height = height;
 
-        button.addEventListener("click", (evt) => {
-            console.log(evt);
-            console.log(key);
-            console.log(value);
-
-        });
-        document.getElementById("controls").appendChild(button);
+        button.addEventListener("click", toggleGraph);
+        controls.appendChild(button);
 
     };
 
-    Object.entries(names).forEach(([key, value]) => {
-        let graph = {
-            color: colors[key],
-            name: names[key],
-            type: types[key],
-            data: {
-                x: columns.find(col => col.includes("x")),
-                y: columns.find(col => col.includes(key))
-            }
-        };
+    /*Canvas manipulations*/
+    const draw = () => {
+        Object.values(graphs).forEach(drawGraph);
+    };
 
-        graphs.push(graph);
+    /*DOM manipulations*/
+    const end = (mainImg) => {
+        main.appendChild(mainImg);
+        Object.values(buttons).forEach(drawButton);
+    };
 
-        drawControls(key, value, colors[key]);
-        drawGraph(graph);
-    });
-
-
-
-
-    end(mainImg);
+    init();
 
 };
-
-const deleteGraph = (data) => {
-
-};
-
-const end = (mainImg) => {
-    document.getElementById("main").appendChild(mainImg);
-};
-
