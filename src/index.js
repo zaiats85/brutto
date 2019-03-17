cdfd5dd8c8276c5b6dceae20f0478b37ae66775d
zaiats85 <oleg.zaiats@gmail.com>
2019-03-17 17:03:42 +0200

    typik

--- src/index.js
+++ src/index.js
@@ -6,9 +6,44 @@ const url = "data.json";
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 const canavsSize = { width: 1200, height: 800 };
 const buttonSize = { width: "140px", height: "50px" };
+const xmlns = "http://www.w3.org/2000/svg";
 
 let controls = document.getElementById("controls");
 let main = document.getElementById("main");
+const SVG_PATH = "check_circle.svg";
+
+/*window.onload=function() {
+    // Get the Object by ID
+    var a = document.getElementById("icon-y0");
+    // Get the SVG document inside the Object tag
+    var svgDoc = a.contentDocument;
+    // Get one of the SVG items by ID;
+    var svgItem = svgDoc.getElementById("fillme");
+    // Set the colour to something else
+    svgItem.setAttribute("fill", "red");
+};*/
+
+const createCheckCircle = (color) => {
+    let svg = document.createElementNS(xmlns, "svg");
+    svg.setAttribute("preserveAspectRatio", "none");
+    let path = document.createElementNS(xmlns, "path");
+    path.setAttributeNS(null, "d", "M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z");
+    path.setAttribute("fill", color);
+    svg.appendChild(path);
+    return svg;
+};
+
+class CheckCircleSvg {
+
+    constructor(id){
+        this.tagName = "object";
+        this.type = "image/svg+xml";
+        this.data = SVG_PATH;
+        this.id = id;
+        this.height = "30px";
+        this.width = "30px";
+    }
+}
 
 /*TRANSPORT*/
 function getJson(method, url) {
@@ -36,12 +71,26 @@ function getJson(method, url) {
 }
 
 /* UTILS */
+/*@TODO make it generic for all dom elements*/
+
 const createCanvas = ({ width, height }, id) => {
     let canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
     canvas.id = id;
     return canvas
+};
+
+const createDOMelement = (tag, ...rest) => {
+    const { tagName, data, type, id, width, height} = tag;
+    let elm = document.createElement(tagName);
+    elm.setAttribute("id", id);
+    elm.setAttribute("width", width);
+    elm.setAttribute("height", height);
+    elm.setAttribute("type", type);
+    elm.setAttribute("data", data);
+
+    return elm;
 };
 
 const objectWithoutKey = (object, key) => {
@@ -151,11 +200,20 @@ const parseFeed = (feed) => {
 
     const drawButton = ({id, color, label, size: { width, height }}) => {
         let button = document.createElement('button');
-        button.id = id;
-        button.innerText = label;
-        button.style.background = color;
-        button.style.width = width;
-        button.style.height = height;
+        let icon = createDOMelement(new CheckCircleSvg(`icon-${id}`), color);
+        let cir = createCheckCircle(color);
+        let text = document.createElement("span");
+        text.innerText = label;
+
+        button.setAttribute("id", id);
+
+        var s = new XMLSerializer();
+        var str = s.serializeToString(cir);
+
+        console.log(str);
+
+        button.setAttribute("style", `background-image: url(data:image/svg+xml;utf8,${str}`);
+        button.appendChild(text);
 
         button.addEventListener("click", toggleGraph);
         controls.appendChild(button);
